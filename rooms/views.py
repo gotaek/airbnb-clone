6308f295eb269e973d42e2c15241c6b7f1a18979
gotaek <64013443+gotaek@users.noreply.github.com>
2020-11-12 23:21:58 +0900

    airbnb clone 11122

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,4 @@
-from django.view.generic import ListView, DetailView
-from django.http import Http404
-from django.shortcuts import render  # html을 return할 수 있게 함
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . import models
 
 # view 는 그url을 통해 view로 들어갈때마다 http request를 생성
@@ -18,10 +15,8 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-# http response를 반환해야함
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
