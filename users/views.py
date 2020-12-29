ae12f54792c7a013835d523ce19d8c156f4ec300
gotaek <64013443+gotaek@users.noreply.github.com>
2020-12-29 22:33:04 +0900

    airbnb clone 1229

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
 from django.views import View
+from django.views.generic import FormView
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
@@ -6,22 +8,28 @@ from . import forms
 # 사용자들이 보게 될 곳
 
 
-class LoginView(View):
-    def get(self, request):
-        form = forms.LoginForm(initial={"email": "deriko@naver.com"})
-        return render(request, "users/login.html", {"form": form})
+class LoginView(FormView):
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
 
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(reverse("core:home"))
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
 
-        return render(request, "users/login.html", {"form": form})
+    # def post(self, request):
+    #     form = forms.LoginForm(request.POST)
+    #     if form.is_valid():
+    #
+    #         if user is not None:
+    #             login(request, user)
+    #             return redirect(reverse("core:home"))
+
+    #     return render(request, "users/login.html", {"form": form})
 
 
 def log_out(request):
